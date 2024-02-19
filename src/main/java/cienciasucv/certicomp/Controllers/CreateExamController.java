240632c500fb6cf28175c49dcf651fa7ca8240e9
Jhonny <zavalajhonny1033@gmail.com>
2024-02-18 21:43:10 -0400

    subiendo adelantos

--- src/main/java/cienciasucv/certicomp/Controllers/CreateExamController.java
+++ src/main/java/cienciasucv/certicomp/Controllers/CreateExamController.java
@@ -1,5 +1,79 @@
 package cienciasucv.certicomp.Controllers;
+import cienciasucv.certicomp.Models.*;
+import cienciasucv.certicomp.Views.AdminViews.*;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonObject;
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class CreateExamController {
+
+    private String generateExamId() {
+        int nextId = 1;
+        File file = new File("exams.json");
+        if (file.exists()) {
+            try (FileReader reader = new FileReader(file)) {
+                JsonObject exams = new Gson().fromJson(reader, JsonObject.class);
+                for (String examID : exams.keySet()) {
+                    int currentIdNumber = Integer.parseInt(examID.substring(1));
+                    nextId = Math.max(nextId, currentIdNumber + 1);
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return "E" + String.format("%03d", nextId);
+    }
+
+    public void createNewExam(Exam exam) {
+
+        Gson gson = new GsonBuilder().setPrettyPrinting().create();
+        JsonObject exams = new JsonObject();
+        File file = new File("exams.json");
+        if (file.exists()) {
+
+            try (FileReader reader = new FileReader(file)) {
+                exams = new Gson().fromJson(reader, JsonObject.class);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        exams.add(exam.getID(), gson.toJsonTree(exam.getAttributes()));
     
+        try (FileWriter writer = new FileWriter("exams.json")) {
+            gson.toJson(exams, writer);
+            System.out.println("Examen creado exitosamente.");
+        } catch (IOException e) {
+            System.out.println("Error, examen no creado." + e.getMessage());
+        }
+    }
+
+    public void collectExamData(CreateExamPanel view){
+
+        String id = generateExamId();
+        String instructions= view.getInstructionsArea();
+        String duration=String.valueOf(view.getDurationBox());
+        ArrayList<String> questions = new ArrayList<>();
+
+        Map<String, Object> examAttributes = new HashMap<>();
+
+        examAttributes.put("duration", view.getDurationBox());
+        examAttributes.put("instructions", view.getInstructionsArea());
+        examAttributes.put("title", view.getNameBox());
+        examAttributes.put("course", view.getCourseBox());
+        
+        Exam exam = new Exam(id,duration, instructions,questions);
+
+        createNewExam(exam);
+
+    }
+  
 }
--- src/main/java/cienciasucv/certicomp/Views/AdminViews/CreateExamView.java
+++ src/main/java/cienciasucv/certicomp/Views/AdminViews/CreateExamView.java
@@ -1,9 +1,37 @@
 package cienciasucv.certicomp.Views.AdminViews;
+import javax.swing.*;
 
-public class CreateExamView {
-
-    CreateExamView(){
-        
-    }
+public class CreateExamView extends JFrame{
     
+    private static CreateExamView instance= null;
+    protected static CreateExamPanel panel;
+    CreateExamView(){
+        panel = new CreateExamPanel();
+        this.setBounds(80, 20, 640, 800);
+        this.setVisible(true);
+        this.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        this.setResizable(false);
+        this.setTitle("Administrador|Crear Examen");
+        this.add(panel);
+    }
+
+    public static CreateExamView getCreateExamView(){
+        if(instance==null){
+            instance = new CreateExamView();
+        }
+        instance.setVisible(true);
+        return instance; 
+    }
+   
+    public static void destroyInstance(){
+        if (instance !=null){
+            instance.dispose();
+            instance=null;
+        }
+    }
+    @Override
+    public void dispose(){
+        panel.restartAll();
+        super.dispose();
+    }
 }
